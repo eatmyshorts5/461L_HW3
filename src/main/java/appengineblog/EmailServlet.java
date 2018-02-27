0c11e0a08cdcb000cc54bb0d65848ce72bf62bd5
Itachiark <chandaarka@yahoo.com>
2018-02-26 23:11:48 -0600

    ficing email

--- src/main/java/appengineblog/EmailServlet.java
+++ src/main/java/appengineblog/EmailServlet.java
@@ -1,16 +1,16 @@
 package appengineblog;
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
-import com.sendgrid.Content;
-import com.sendgrid.Email;
-import com.sendgrid.Mail;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
 
 
 
@@ -20,7 +20,7 @@ public class EmailServlet extends HttpServlet {
 
 public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
 {
-	Email from = new Email("test@example.com");
+	/*Email from = new Email("test@example.com");
     String subject = "Sending with SendGrid is Fun";
     Email to = new Email("test@example.com");
     Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
@@ -38,6 +38,46 @@ public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOExc
       System.out.println(response.getHeaders());
     } catch (IOException ex) {
       throw ex;
+    }*/
+	// Recipient's email ID needs to be mentioned.
+    String to = "chandaarka@yahoo.com";
+
+    // Sender's email ID needs to be mentioned
+    String from = "chandaarka@yahoo.com";
+
+    // Assuming you are sending email from localhost
+    String host = "localhost";
+
+    // Get system properties
+    //Properties properties = System.getProperties();
+
+    // Setup mail server
+    //properties.setProperty("mail.smtp.host", host);
+    Properties props = new Properties();
+    // Get the default Session object.
+    Session session = Session.getDefaultInstance(props, null);
+
+    try {
+       // Create a default MimeMessage object.
+       MimeMessage message = new MimeMessage(session);
+
+       // Set From: header field of the header.
+       message.setFrom(new InternetAddress(from));
+
+       // Set To: header field of the header.
+       message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+       // Set Subject: header field
+       message.setSubject("This is the Subject Line!");
+
+       // Now set the actual message
+       message.setText("This is actual message");
+
+       // Send message
+       Transport.send(message);
+       System.out.println("Sent message successfully....");
+    } catch (MessagingException mex) {
+       mex.printStackTrace();
     }
 }
 
