b1e39fdecf7a739778cbe4ffd5ec1a777e6b9773
Itachiark <chandaarka@yahoo.com>
2018-02-27 00:34:38 -0600

    email to sub list works

--- src/main/java/appengineblog/EmailServlet.java
+++ src/main/java/appengineblog/EmailServlet.java
@@ -1,5 +1,9 @@
 package appengineblog;
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -45,8 +49,11 @@ public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOExc
     } catch (IOException ex) {
       throw ex;
     }*/
+	
+	List<Subscriber> subscribers = ofy().load().type(Subscriber.class).list();
+	
 	// Recipient's email ID needs to be mentioned.
-    String to = "chandaarka@yahoo.com";
+    //String to = "chandaarka@yahoo.com";
 
     // Sender's email ID needs to be mentioned
     String from = "arka.chanda2752@gmail.com";
@@ -62,7 +69,8 @@ public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOExc
     Properties props = new Properties();
     // Get the default Session object.
     Session session = Session.getDefaultInstance(props, null);
-
+    for(Subscriber sub :  subscribers){
+		String to = sub.getUser().getEmail();
     try {
        // Create a default MimeMessage object.
        MimeMessage message = new MimeMessage(session);
@@ -74,16 +82,27 @@ public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOExc
        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
        // Set Subject: header field
-       message.setSubject("This is the Subject Line!");
-
+       message.setSubject("New Blog Posts!! :)");
+       String mes = null;
        // Now set the actual message
-       message.setText("This is actual message");
+       List<BlogPost> posts = ofy().load().type(BlogPost.class).list();
+       Date yes = new Date();
+       long current = yes.getTime();
+       for(BlogPost meme : posts){
+    	   if((current - meme.getDate().getTime()) < 86400000)
+    	   {
+    		   mes = mes + meme.getPost() + "/n/n";
+    	   }
+       }
+       if(mes.equals(null)){return;}
+       message.setText(mes); 
 
        // Send message
        Transport.send(message);
        System.out.println("Sent message successfully....");
     } catch (MessagingException mex) {
        mex.printStackTrace();
+    }
     }
 }
 
