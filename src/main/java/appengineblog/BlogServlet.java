f09817371d99fa3525baa0d3f8477cf6851edf68
Itachiark <chandaarka@yahoo.com>
2018-02-26 21:30:13 -0600

    new file for email

--- src/main/java/appengineblog/BlogServlet.java
+++ src/main/java/appengineblog/BlogServlet.java
@@ -33,42 +33,6 @@ public class BlogServlet extends HttpServlet {
 
 	}
 	
-	private static final Logger _logger = Logger.getLogger(BlogServlet.class.getName());
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-			try {
-			_logger.info("Cron Job has been executed");
-			//Put your logic here
-			//BEGIN
-			
-			Email from = new Email("test@example.com");
-		    String subject = "Sending with SendGrid is Fun";
-		    Email to = new Email("chandaarka@yahoo.com");
-		    Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
-		    Mail mail = new Mail(from, subject, to, content);
-		    
-		    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-		    Request request = new Request();
-		    try {
-		      request.setMethod(Method.POST);
-		      request.setEndpoint("mail/send");
-		      request.setBody(mail.build());
-		      Response response = sg.api(request);
-		      System.out.println(response.getStatusCode());
-		      System.out.println(response.getBody());
-		      System.out.println(response.getHeaders());
-		    } catch (IOException ex) {
-		      throw ex;
-		    }
-		    
-			//END
-			}
-			catch (Exception ex) {
-			//Log any exceptions in your Cron Job
-			}
-			}
-	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		/*UserService userService = UserServiceFactory.getUserService();
 		User user = userService.getCurrentUser();
